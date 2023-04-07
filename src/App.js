167c80b76d51da28d73b521570f7a0102a356cc4
mamunur1296 <programer597@gmail.com>
2023-04-07 14:32:26 +0600

    hello

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 function App() {
-  return <h1>Hello world in the home !</h1>;
+  return (
+    <h1 className="text-2lg text-orange-500 text-center">
+      Hello world in the home and i am a teacher in this cors !
+    </h1>
+  );
 }
 
 export default App;
